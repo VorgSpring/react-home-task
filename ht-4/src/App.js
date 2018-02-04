7561ff1122d2fba5345083df94f4186348aa2e5d
VorgSpring <drmrSP@yandex.ru>
2018-02-04 22:33:10 +0300

    performed fourth home task

--- ht-4/src/App.js
+++ ht-4/src/App.js
@@ -1,6 +1,11 @@
-import React, {Component} from 'react';
-import './App.css';
-import {addListener, removeListener, isAuthorized} from './AuthorizeApi';
+import React, {Component, Fragment} from 'react'
+import './App.css'
+import {addListener, removeListener, isAuthorized} from './AuthorizeApi'
+import {Link, Route, Switch, Redirect} from 'react-router-dom'
+import Home from './Home'
+import Private from './Private'
+import Public from './Public'
+import Auth from './Auth'
 
 class App extends Component {
   state = {
@@ -8,19 +13,39 @@ class App extends Component {
   };
 
   componentDidMount() {
-    addListener(this.handleAuthorize);
+    addListener(this.handleAuthorize)
   }
 
   componentWillUnmount() {
-    removeListener(this.handleAuthorize);
+    removeListener(this.handleAuthorize)
   }
 
   handleAuthorize = isAuthorized => {
-    this.setState({isAuthorized});
-  };
+    this.setState({isAuthorized})
+  }
 
   render() {
-    return null;
+    const {isAuthorized} = this.state
+    return (
+      <Fragment>
+        <nav>
+          <ul>
+            <li><Link to='/auth'>Войти</Link></li>
+            <li><Link to='/private'>Секретная страница</Link></li>
+            <li><Link to='/public'>Публичная страница</Link></li>
+            <li><Link to='/'>Главная</Link></li>
+          </ul>
+        </nav>
+    
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/auth' component={Auth} />
+          <Route path='/public' component={Public} />
+          {isAuthorized ? <Route path='/private' component={Private} /> : <Redirect from='/private' to='/auth' />}
+          <Redirect from='*' to='/' />
+        </Switch>
+      </Fragment>
+    )
   }
 }
 
--- ht-4/src/Auth.js
+++ ht-4/src/Auth.js
@@ -1,9 +1,49 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react'
+import { authorizeUser, isAuthorized } from './AuthorizeApi'
+import { Redirect } from 'react-router-dom'
 
 class Auth extends Component {
+  state = {
+    isAuthorized,
+    isValid: true,
+    email: '',
+    password: ''
+  }
+
+  onChange = (event) => {
+    const {name, value} = event.target
+        
+    this.setState({
+        [name]: value
+    })
+  }
+
+  handleSubmit = () => {
+    const {email, password} = this.state
+    const isAuthorized = authorizeUser(email, password)
+    this.setState({
+      isAuthorized,
+      isValid: isAuthorized,
+      email: '',
+      password: ''
+    })
+  }
+
   render() {
-    return null;
+    const {isAuthorized, isValid, email, password} = this.state
+    
+    return (
+      <Fragment>
+        {isAuthorized && <Redirect to="/" />}
+        <div>
+          <input onChange={this.onChange} type="email" name="email" value={email} placeholder="email" />
+          <input onChange={this.onChange} type="password" name="password" value={password} placeholder="password" />
+          {!isValid && <p className="error">Неверный пароль и/или почта.</p>}
+        </div>
+        <button onClick={this.handleSubmit} type="button">Submit</button>
+      </Fragment>
+    )
   }
 }
 
-export default Auth;
+export default Auth
